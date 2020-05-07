f548241dc0a33eed0fb936b316fb8dfe8eedc6c9
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-07 17:54:41 +0400

    fix

--- autofocus.js
+++ autofocus.js
@@ -1,66 +1,68 @@
 //autofocus:(bool),//Выставлять фокус на первый пустой input в слое
-infrajs.autofocus_moveCaretToEnd=function(inp){
-	try{
-		if (inp.createTextRange){
+infrajs.autofocus_moveCaretToEnd = function (inp) {
+	try {
+		if (inp.createTextRange) {
 			var r = inp.createTextRange();
 			r.collapse(false);
 			r.select();
-		}else if (inp.selectionStart){
+		} else if (inp.selectionStart) {
 			var end = inp.value.length;
-			inp.setSelectionRange(end,end);
+			inp.setSelectionRange(end, end);
 			inp.focus();
-		}else if(typeof(inp.selectionStart) == "number"){
+		} else if (typeof (inp.selectionStart) == "number") {
 			inp.selectionStart = inp.selectionEnd = inp.value.length;
 		}
-	}catch(e){}
+	} catch (e) { }
 }
 infrajs.autofocus = async function (layer) {//onshow
 	let CDN = (await import('/vendor/akiyatkin/load/CDN.js')).default
 	await CDN.load('jquery')
-	if( typeof(layer)=='string' ) {
-		var div=$(layer);
-		var layer={};
+	if (typeof (layer) == 'string') {
+		var div = $(layer);
+		var layer = {};
 	} else {
-		if(!layer.autofocus)return;
-		var div=$('#'+layer.div);
-		div.find('input, textarea').focus(function(){
-			var inp=$(this);
-			layer.autofocuswas=inp.attr('name');	
+		if (!layer.autofocus) return;
+		var div = $('#' + layer.div);
+		div.find('input, textarea').focus(function () {
+			var inp = $(this);
+			layer.autofocuswas = inp.attr('name');
 		});
 	}
 
-	if(layer.autofocuswas){
-		var inp=div.find('[name="'+layer.autofocuswas+'"]');
+	if (layer.autofocuswas) {
+		var inp = div.find('[name="' + layer.autofocuswas + '"]');
 	} else {
 		var inp = $('[autofocus]:first');
 		if (!inp.length) {
 			var inp = div.find('input:first[value=""][type=text]'); //На первый пустой инпут
-			if(!inp.length){
+			if (!inp.length) {
 				var inp = div.find('input:first[value=""][type=number]'); //На первый пустой инпут
 			} else {
 
 			}
-			if(!inp.length){
-				var inp=div.find('input:first[type=text]');
+			if (!inp.length) {
+				var inp = div.find('input:first[type=text]');
 			}
-			if(!inp.length){
-				var inp=div.find('input:first[type=number]');
+			if (!inp.length) {
+				var inp = div.find('input:first[type=number]');
 			}
 		}
 	}
 	//inp.prop('autofocus',true);
 	inp.focus();
-	if(inp.length){
+	if (inp.length) {
 		infrajs.autofocus_moveCaretToEnd(inp.get(0));
 	}
 }
-infrajs.autofocussave = function(layer){//oncheck
+infrajs.autofocussave = async function (layer) {//oncheck
 	//autofocus
-	if(!layer.autofocus)return;
-	if(!layer.showed)return;
-	var div=$('#'+layer.div);
-	var inp=div.find('input:focus');
-	if(!inp.length)return;
-	layer.autofocuswas=inp.attr('name');//Если происходит асинхронный ответ и тп...
+	if (!layer.autofocus) return;
+	if (!layer.showed) return;
+	let { CDN } = await import('/vendor/akiyatkin/load/CDN.js')
+	await CDN.load('jquery')
+	var div = $('#' + layer.div);
+	var inp = div.find('input:focus');
+	if (!inp.length) return;
+	layer.autofocuswas = inp.attr('name');//Если происходит асинхронный ответ и тп...
 }
 
